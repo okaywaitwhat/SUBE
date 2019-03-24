94b2ed2db45e225a66d6907f9c5282c87cb782ef
julietaflux <julietaflux@gmail.com>
2019-03-23 23:57:08 -0300

    First commit

---
+++ SUBE/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace SUBE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader r = new StreamReader("Movements.json"))
+            {
+                string json = r.ReadToEnd();
+                //List<Movement> items = JsonConvert.DeserializeObject<List<Movement>>(json);
+
+                dynamic array = JsonConvert.DeserializeObject(json);
+                foreach (var item in array)
+                {
+                    Console.WriteLine("{0} {1} {2}", item.Date, item.ValueFormat, item.BalanceFormat);
+                }
+            }
+        }
+
+        /*public class Movement
+        {
+            public string Date;
+            public string Id;
+            public string Type;
+            public string Entity;
+            public string Place;
+            public string BalanceFormat;
+            public string ValueFormat;
+            public bool IsNegative;
+            public bool IsNegativeBalance;
+        }*/
+    }
+}
+
---
+++ SUBE/ReadData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace SUBE
+{
+    public class ReadData
+    {
+        public void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("Movements.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Movement> items = JsonConvert.DeserializeObject<List<Movement>>(json);
+
+                dynamic array = JsonConvert.DeserializeObject(json);
+                foreach (var item in array)
+                {
+                    Console.WriteLine("{0} {1}", item.Date, item.ValueFormat);
+                }
+            }
+        }
+
+        public class Movement
+        {
+            public DateTime Date;
+            public int ID;
+            public string Entity;
+            public string Place;
+            public string BalanceFormat;
+            public string ValueFormat;
+        }
+    }
+
+}
